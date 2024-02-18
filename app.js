5f44df19701945475d5c925b5aa83b7b3df98f68
Alon Arad <alonarad@Alons-MacBook-Pro.local>
2024-02-18 12:44:50 -0800

    added more text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('this will host the s3 load button page'));
+app.get('/', (req, res) => res.send('this will host the s3 load button page, timmy will be proud of this full CI/CD pipeline that I put in 2 hours'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
